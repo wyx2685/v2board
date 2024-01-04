21b1bfbcfdb4e23d717a69d21f2a1db66b7d9664
lolly <purhays@gmail.com>
2024-01-04 14:43:03 +0800

    Update UserController.php

--- app/Http/Controllers/V1/Admin/UserController.php
+++ app/Http/Controllers/V1/Admin/UserController.php
@@ -59,7 +59,7 @@ class UserController extends Controller
         $current = $request->input('current') ? $request->input('current') : 1;
         $pageSize = $request->input('pageSize') >= 10 ? $request->input('pageSize') : 10;
         $sortType = in_array($request->input('sort_type'), ['ASC', 'DESC']) ? $request->input('sort_type') : 'DESC';
-        $sort = $request->input('sort') ? $request->input('sort') : 'created_at';
+        $sort = $request->input('sort') ? $request->input('sort') : 'id';
         $userModel = User::select(
             DB::raw('*'),
             DB::raw('(u+d) as total_used')
