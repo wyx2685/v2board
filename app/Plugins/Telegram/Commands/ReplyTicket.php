6d2d6bbbef952335c3cc53ed12612786bef7f3c4
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2024-09-13 18:57:24 +0330

    ترجمه

--- app/Plugins/Telegram/Commands/ReplyTicket.php
+++ app/Plugins/Telegram/Commands/ReplyTicket.php
@@ -7,31 +7,44 @@ use App\Plugins\Telegram\Telegram;
 use App\Services\TicketService;
 
 class ReplyTicket extends Telegram {
-    public $regex = '/[#](.*)/';
-    public $description = '快速工单回复';
+    public $regex = '/[#](.*)/';  // الگوی مورد نیاز برای شناسایی تیکت‌ها از پیام
+    public $description = 'پاسخ سریع به تیکت‌ها';
 
     public function handle($message, $match = []) {
+        // فقط پیام‌های خصوصی مجاز هستند
         if (!$message->is_private) return;
+        
+        // فراخوانی تابع پاسخ به تیکت با استفاده از شناسه تیکت
         $this->replayTicket($message, $match[1]);
     }
 
-
     private function replayTicket($msg, $ticketId)
     {
+        // یافتن کاربر با استفاده از شناسه تلگرام
         $user = User::where('telegram_id', $msg->chat_id)->first();
         if (!$user) {
-            abort(500, '用户不存在');
+            abort(500, 'کاربر وجود ندارد');  // خطای 500 در صورت عدم وجود کاربر
         }
+
+        // بررسی این که متن پیام موجود باشد
         if (!$msg->text) return;
+
+        // بررسی این که کاربر ادمین یا کارمند باشد
         if (!($user->is_admin || $user->is_staff)) return;
+
+        // پاسخ به تیکت با استفاده از سرویس تیکت
         $ticketService = new TicketService();
         $ticketService->replyByAdmin(
             $ticketId,
             $msg->text,
             $user->id
         );
+
+        // ارسال پیام موفقیت به کاربر
         $telegramService = $this->telegramService;
-        $telegramService->sendMessage($msg->chat_id, "#`{$ticketId}` 的工单已回复成功", 'markdown');
-        $telegramService->sendMessageWithAdmin("#`{$ticketId}` 的工单已由 {$user->email} 进行回复", true);
+        $telegramService->sendMessage($msg->chat_id, "تیکت شماره #`{$ticketId}` با موفقیت پاسخ داده شد", 'markdown');
+        
+        // اطلاع‌رسانی به ادمین‌ها
+        $telegramService->sendMessageWithAdmin("تیکت شماره #`{$ticketId}` توسط {$user->email} پاسخ داده شد", true);
     }
 }
