92c1c283e3a8309b09786e5835d4a152efcaafad
MeTerminator <42143735+MeTerminator@users.noreply.github.com>
2024-12-01 01:08:39 +0800

    精简信息，维持稳定性
    弃繁从简

--- app/Http/Controllers/V1/Client/ClientController.php
+++ app/Http/Controllers/V1/Client/ClientController.php
@@ -50,44 +50,16 @@ class ClientController extends Controller
             return $class->handle();
         }
     }
-
     private function setSubscribeInfoToServers(&$servers, $user)
     {
         if (!isset($servers[0])) return;
         if (!(int)config('v2board.show_info_to_server_enable', 0)) return;
-        // MeT 20240903
-        //统计在线设备
-        $countalive = 0;
-        $ips = [];
-        $ips_array = Cache::get('ALIVE_IP_USER_'. $user['id']);
-        if ($ips_array) {
-            $countalive = $ips_array['alive_ip'];
-            foreach($ips_array as $nodetypeid => $data) {
-                if (!is_int($data) && isset($data['aliveips'])) {
-                    foreach($data['aliveips'] as $ip_NodeId) {
-                        $ip = explode("_", $ip_NodeId)[0];
-                        $ips[] = $ip . '_' . $nodetypeid;
-                    }
-                }
-            }
-        }
-        $user['alive_ip'] = $countalive;
-        
-        $upload = round($user['u'] / (1024*1024*1024), 2);
-        $download = round($user['d'] / (1024*1024*1024), 2);
         $useTraffic = $user['u'] + $user['d'];
         $totalTraffic = $user['transfer_enable'];
         $remainingTraffic = Helper::trafficConvert($totalTraffic - $useTraffic);
         $expiredDate = $user['expired_at'] ? date('Y-m-d', $user['expired_at']) : '长期有效';
         $userService = new UserService();
         $resetDay = $userService->getResetDay($user);
-        $deviceLimit = $user['device_limit'] ? $user['device_limit'] : '∞';
-
-        array_unshift($servers, array_merge($servers[0], [
-            'name' => "🚀设备限制：{$user['alive_ip']} / {$deviceLimit}",
-            'host' => '127.0.0.1',
-            'port' => 10086,
-        ]));
         array_unshift($servers, array_merge($servers[0], [
             'name' => "🚀套餐到期：{$expiredDate}",
             'host' => '127.0.0.1',
@@ -102,16 +74,6 @@ class ClientController extends Controller
         }
         array_unshift($servers, array_merge($servers[0], [
             'name' => "🚀剩余流量：{$remainingTraffic}",
-            'host' => '127.0.0.1',
-            'port' => 10086,
-        ]));
-        array_unshift($servers, array_merge($servers[0], [
-            'name' => "🚀已用流量：上传 {$upload}GB | 下载 {$download}GB",
-            'host' => '127.0.0.1',
-            'port' => 10086,
-        ]));
-        array_unshift($servers, array_merge($servers[0], [
-            'name' => "🚀邮箱：{$user['email']}",
             'host' => '127.0.0.1',
             'port' => 10086,
         ]));
