9519751903cd33dee0a28abfcb773c14e9f59b88
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2025-05-20 11:10:58 +0330

    Update Singbox.php

--- app/Protocols/Singbox/Singbox.php
+++ app/Protocols/Singbox/Singbox.php
@@ -81,7 +81,7 @@ class Singbox
     protected function addProxies($proxies)
     {
         foreach ($this->config['outbounds'] as &$outbound) {
-            if (($outbound['type'] === 'selector' && $outbound['tag'] === '节点选择') || ($outbound['type'] === 'urltest' && $outbound['tag'] === '自动选择') || ($outbound['type'] === 'selector' && strpos($outbound['tag'], '#') === 0 )) {
+            if (($outbound['type'] === 'selector' && $outbound['tag'] === 'دکتر‌موبایل‌جایزان') || ($outbound['type'] === 'urltest' && $outbound['tag'] === 'انتخاب خودکار') || ($outbound['type'] === 'selector' && strpos($outbound['tag'], '#') === 0 )) {
                 array_push($outbound['outbounds'], ...array_column($proxies, 'tag'));
             }
         }
