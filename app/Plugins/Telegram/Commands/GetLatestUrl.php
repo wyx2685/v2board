5195d90927c44588119200b887cba713889a6712
Ahmad10611 <106405309+Ahmad10611@users.noreply.github.com>
2025-06-12 17:12:09 +0330

    Update GetLatestUrl.php

--- app/Plugins/Telegram/Commands/GetLatestUrl.php
+++ app/Plugins/Telegram/Commands/GetLatestUrl.php
@@ -7,12 +7,12 @@ use App\Plugins\Telegram\Telegram;
 
 class GetLatestUrl extends Telegram {
     public $command = '/getlatesturl';
-    public $description = '将Telegram账号绑定到网站';
+    public $description = 'دریافت آخرین آدرس وب‌سایت';
 
     public function handle($message, $match = []) {
         $telegramService = $this->telegramService;
         $text = sprintf(
-            "%s的最新网址是：%s",
+            "آخرین آدرس %s: %s",
             config('v2board.app_name', 'V2Board'),
             config('v2board.app_url')
         );
