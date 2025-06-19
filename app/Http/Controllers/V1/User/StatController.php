3c60eb59960e7a4cd4bd597c41e7f5f22f6dafb3
kutycma <khacquang2010@gmail.com>
2025-06-19 23:29:48 +0700

    update dùng theme new

--- app/Http/Controllers/V1/User/StatController.php
+++ app/Http/Controllers/V1/User/StatController.php
@@ -11,18 +11,20 @@ class StatController extends Controller
 {
     public function getTrafficLog(Request $request)
     {
-        $builder = StatUser::select([
-            'u',
-            'd',
+        $userId = $request->user['id'];
+
+        $builder = StatUser::selectRaw(
+            'u * server_rate AS u',
+            'd * server_rate AS d',
             'record_at',
             'user_id',
-            'server_rate'
-        ])
-            ->where('user_id', $request->user['id'])
-            ->where('record_at', '>=', strtotime(date('Y-m-1')))
-            ->orderBy('record_at', 'DESC');
-        return response([
-            'data' => $builder->get()
-        ]);
+            DB::raw('1 AS server_rate')
+        )
+        ->where('user_id', $userId)
+        ->where('record_at', '>=', strtotime(date('Y-m-1')))
+        ->orderBy('record_at', 'DESC');
+
+        $data = $builder->get();
+        return response(['data' => $data]);
     }
 }
