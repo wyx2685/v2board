32f859c5e7af8e489e45d0c84098289a3fe1e8f6
kexue-aihao <weacrcr@proton.me>
2026-06-01 22:55:58 +0800

    chore: v2node 安装命令指向 kexue-aihao/v2node

--- app/Services/ServerService.php
+++ app/Services/ServerService.php
@@ -395,7 +395,7 @@ class ServerService
             $apiHostArg = escapeshellarg((string) $apiHost);
             $apiKeyArg = escapeshellarg((string) $apiKey);
             $servers[$k]['install_command'] = sprintf(
-                'wget -N https://raw.githubusercontent.com/wyx2685/v2node/master/script/install.sh && bash install.sh --api-host %s --node-id %d --api-key %s',
+                'wget -N https://raw.githubusercontent.com/kexue-aihao/v2node/master/script/install.sh && bash install.sh --api-host %s --node-id %d --api-key %s',
                 $apiHostArg,
                 $nodeId,
                 $apiKeyArg
