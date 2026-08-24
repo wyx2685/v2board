25ab8e081c3dbcdae365bbed558aff63695d4675
wyx2685 <wyx2685@gmail.com>
2026-08-25 04:45:00 +0900

    test: 添加pinned_peer_cert_sha256 订阅参数

--- app/Utils/Helper.php
+++ app/Utils/Helper.php
@@ -242,6 +242,7 @@ class Helper
             $tlsSettings = $server['tls_settings'] ?? $server['tlsSettings'] ?? [];
             $config['allowInsecure'] = (int)($tlsSettings['allow_insecure'] ?? $tlsSettings['allowInsecure'] ?? 0);
             $config['sni'] = $tlsSettings['server_name'] ?? $tlsSettings['serverName'] ?? '';
+            $config['pcs'] = $tlsSettings['pinned_peer_cert_sha256'] ?? '';
         }
         
         $network = (string)$server['network'];
@@ -306,6 +307,7 @@ class Helper
             "flow" => $server['flow'],
             "fp" => $tlsSettings['fingerprint'] ?? 'chrome',
             "insecure" => $tlsSettings['allow_insecure'] ?? 0,
+            "pcs" => $tlsSettings['pinned_peer_cert_sha256'] ?? '',
         ];
 
         if ($server['tls']) {
@@ -345,6 +347,7 @@ class Helper
             'allowInsecure' => $server['allow_insecure'] ?? ($tlsSettings['allow_insecure'] ?? 0),
             'peer' => $server['server_name'] ?? ($tlsSettings['server_name'] ?? ''),
             'sni' => $server['server_name'] ?? ($tlsSettings['server_name'] ?? ''),
+            'pcs' => $tlsSettings['pinned_peer_cert_sha256'] ?? '',
             'type'=> $server['network'],
         ];
 
@@ -406,7 +409,8 @@ class Helper
         $tlsSettings = $server['tls_settings'] ?? [];
         $insecure = $tlsSettings['allow_insecure'] ?? 0;
         $sni = $tlsSettings['server_name'] ?? '';
-        $uri = "hysteria2://{$password}@{$remote}:{$firstPort}/?insecure={$insecure}&sni={$sni}";
+        $pcs = $tlsSettings['pinned_peer_cert_sha256'] ?? '';
+        $uri = "hysteria2://{$password}@{$remote}:{$firstPort}/?insecure={$insecure}&sni={$sni}&pcs={$pcs}";
 
         if (isset($server['obfs']) && isset($server['obfs_password'])) {
             $obfs_password = rawurlencode($server['obfs_password']);
@@ -428,6 +432,7 @@ class Helper
             'allow_insecure' => $server['insecure'] ?? ($tlsSettings['allow_insecure'] ?? 0),
             'disable_sni' => $server['disable_sni'],
             'udp_relay_mode' => $server['udp_relay_mode'],
+            'pcs' => $tlsSettings['pinned_peer_cert_sha256'] ?? '',
         ];
 
         $remote = self::formatHost($server['host']);
@@ -445,6 +450,7 @@ class Helper
             'type' => $server['network'] ?? 'tcp',
             'insecure' => $server['insecure'] ?? ($tlsSettings['allow_insecure'] ?? 0),
             'fp' => $tlsSettings['fingerprint'] ?? 'chrome',
+            'pcs' => $tlsSettings['pinned_peer_cert_sha256'] ?? '',
         ];
         if (isset($server['server_name']) || isset($tlsSettings['server_name'])) {
             $config['sni'] = $server['server_name'] ?? ($tlsSettings['server_name'] ?? '');
